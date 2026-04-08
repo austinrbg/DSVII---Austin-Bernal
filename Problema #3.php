09508d72b0e892645f113c760d8877f2ecb91333
austinrbg <austinr.bernal@gmail.com>
2026-04-08 10:46:52 -0500

    Update Problema #3.php

--- Problema #3.php
+++ Problema #3.php
@@ -2,5 +2,15 @@
     $numero1 = 7;
     $numero2 = 3;
     $resultado = $numero1 * $numero2; //$resultado contendrá 21
-    echo "El resultado de la multiplicación es: " . $resultado;
+    echo "El resultado de la multiplicación es: " . $resultado . "\n";
+
+    $numero3 = 2;
+    $numero4 = 3;
+    $numero5 = 4;
+
+    $resultado3 = $numero3 + $numero4 * $numero5; 
+    echo "\nEl resultado de la operación sin paréntesis es: " . $resultado3;
+
+    $resultado2 = ($numero3 + $numero4) * $numero5; 
+    echo "\n\nEl resultado de la operación con paréntesis es: " . $resultado2;
 ?>
