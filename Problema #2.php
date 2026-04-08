0f6d19422a39f46c5428542c0414bac0163d105d
austinrbg <austinr.bernal@gmail.com>
2026-04-08 10:36:42 -0500

    Problemas practica
    3 problemas de la practica sumativa

---
+++ Problema #2.php
@@ -0,0 +1,6 @@
+<?php
+    $numero1 = 10;
+    $numero2 = 4;
+    $resultado = $numero1 - $numero2; //$resultado contendrá 6
+    echo "El resultado de la resta es: " . $resultado;
+?>
---
+++ Problema #3.php
@@ -0,0 +1,6 @@
+<?php
+    $numero1 = 7;
+    $numero2 = 3;
+    $resultado = $numero1 * $numero2; //$resultado contendrá 21
+    echo "El resultado de la multiplicación es: " . $resultado;
+?>
